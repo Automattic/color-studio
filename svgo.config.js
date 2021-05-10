262bf94d8ab509c688bb83493e02db81abe39f67
Dominik Porada <dominik@porada.co>
2021-05-10 14:29:46 +0200

    Update SVGO

---
+++ svgo.config.js
@@ -0,0 +1,22 @@
+const { extendDefaultPlugins } = require('svgo')
+
+module.exports = {
+  multipass: true,
+  plugins: extendDefaultPlugins([
+    {
+      name: 'cleanupNumericValues',
+      params: {
+        floatPrecision: 2
+      }
+    },
+    {
+      name: 'convertPathData',
+      params: {
+        floatPrecision: 2
+      }
+    },
+    'removeOffCanvasPaths',
+    'removeRasterImages',
+    'sortAttrs'
+  ])
+}
